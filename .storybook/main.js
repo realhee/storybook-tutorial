95f01e35815667597dda0c097ce68c461fb8ef1d
realhee <wlsgml5628@gmail.com>
2023-02-15 00:28:01 +0900

    create Complex Component

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,14 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
+  features: {
+    postcss: false,
+  },
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
-  },
-  features: {
-    interactionsDebugger: true,
-    postcss: false,
   },
 };
